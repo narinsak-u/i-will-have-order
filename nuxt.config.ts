c107e78ea02e8775d81f55a2ef9f1f8b3aae06e7
narinsak.u <nawongsak@gmail.com>
2026-05-26 19:18:59 +0700

    feat: add IBM Plex Sans Thai font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@300;400;500;600;700&display=swap' }
+      ]
+    }
+  },
   vite: {
     plugins: [
       tailwindcss()
